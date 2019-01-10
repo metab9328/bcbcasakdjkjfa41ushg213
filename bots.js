0831b71ae2c924aefe2c09f4aaa8d4fbfd4e62f6
metab9328 <46504069+metab9328@users.noreply.github.com>
2019-01-10 07:55:57 +0300

    Rename bot.js to bots.js

---
+++ bots.js
@@ -0,0 +1,21 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+console.log("Discord Bot By So.?");
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+
+
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (author) {
+  return author.send(' ** حرام واحد جميل مثلك م يكون عندنا .. تنورنا  https://discord.gg/zz6MdDy ** ')
+
+}).catch(console.error)
+})
+
+client.login("NTMyMzAwNzU5NDAwNzEwMTU0.DxafUw.BDPD0KX7t3-uJiZRrjoaB4c4nng");
